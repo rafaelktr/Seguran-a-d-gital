ced7cc1773ec3986a53309fefe6e934d3e9ade40
rafaelktr <127497544+rafaelktr@users.noreply.github.com>
2024-08-22 14:48:46 -0300

    a

--- java/quetao.js
+++ java/quetao.js
@@ -1,10 +1,185 @@
 export const perguntas = [
+    
+    export const perguntas = [
+        {
+            enunciado: "O sol está quente. Até demais. O primeiro feixo de luz que bate o seu rosto em quase uma década. Ou, pelo menos é o que você pensa. Apesar de tudo, você ainda se lembra dos caminhos afrente, e como eles te levam pela foresta. Igual a suas escolhas pasadas, redundantes, pois os dois te levarão ao mesmo local.",
+            alternativas: [
+                {
+                    texto: "Ir pelo caminho limpo.",
+                    afirmacao: [
+                        "█████ foi um covarde. Desgraça a raça humana."
+                    ],
+                    proxima: 1,
+                },
+                {
+                    texto: "Ir pelo caminho denso.",
+                    afirmacao: [
+                        "█████ tinha coragem ínutil. Algo que o levou a nada."
+                    ],
+                    proxima: 2,
+                },
+            ]
+        },
+        {
+            enunciado: "Um coelho está na sua frente. Preso em uma armadilha de urso não feita para ele. Sua perna esta quebrada, sangrando, mas ainda vivo. Salva-lo não ira te trazer nada, pois ele vai morrer de uma forma ou outra. Seja sangrando aqui, ou para fora.",
+            alternativas: [
+                {
+                    texto: "Liberte-o.",
+                    afirmacao: [
+                        "█████ odiava dor, seja em si ou em outros. Sacrificou tempo para se satisfazer."
+                    ],
+                    proxima: 3,
+                },
+                {
+                    texto: "Abandone-o.",
+                    afirmacao: [
+                        "█████ adorava ver dor, especialmente as dos outros. Egoismo que não acaba mais."
+                    ],
+                    proxima: 3,
+                },
+            ]
+        },
+        {
+            enunciado: "Seus passos finalmente te levou a um lugar inqueto. Tem um corpo na sua frente, e animais estão pegando os nutrientes nescessários para sua própria sobrevivência. Você não sente nada pelo corpo (mesmo o reconhecendo), mas uma parte de sua mente te deseja que faça o que você deseja.",
+            alternativas: [
+                {
+                    texto: "Deixe a floresta o conquistar.",
+                    afirmacao: [
+                        "Prefiriu abandonar amigos para o bem de outros. Honorável pelas razões erradas."
+                    ],
+                    proxima: 4,
+                },
+                {
+                    texto: "De honra a pessoa morta.",
+                    afirmacao: [
+                        "Colocou amigos acima de outros. Muito honorável para seu próprio bem."
+                    ],
+                    proxima: 4,
+                },
+            ]
+        },
+        {
+            enunciado: "Tem uma parede enorme na sua frente. Vista a quilômetros de distância. Tem uma porta be na base, com duas torretas olhando de lado a outro, enferrugadas mas ainda funcionando.",
+            alternativas: [
+                {
+                    texto: "Caminhar até elas com calma.",
+                    afirmacao: [
+                        "Achou que tinha uma escolha."
+                    ],
+                    proxima: 7,
+                },
+                {
+                    texto: "Correr para passar pela porta.",
+                    afirmacao: [
+                        "Aceitou a morte que veio."
+                    ],
+                    proxima: 7,
+                },
+            ]
+        },
+        {
+            enunciado: "Um grito é ouvido na floresta, em algum local entre as árvores. Um sentimento primordial dentro de você te fala que está sendo visto, mas você não sabe da onde. Em segundos, passos aceleram até você correndo entre as folhas mortas.",
+            alternativas: [
+                {
+                    texto: "Correr para esquerda.",
+                    afirmacao: [
+                        "████ nunca foi bom em decisôes."
+                    ],
+                    proxima: 5,
+                },
+                {
+                    texto: "Correr para a direita.",
+                    afirmacao: [
+                        "████ trabalha mal em situações de alta pressão."
+                    ],
+                    proxima: 6,
+                },
+            ]
+        },
+        {
+            enunciado: "Você cai em uma armadilha escondida por folhas. Em meio segundo, tudo fica devagar. O tempo pareçe estar em camera lenta em sua vista. Espinhos de madeira estão no final desse buraco, colocados para cima em esperança de ter algo cair em cima. Depois de cair, seu corpo dói, mas continua vivo. Nos últimos segundos, você pode ver uma figura rindo de sua situação, olhando para baixo, diretamente acima do buraco.",
+            alternativas: [
+                {
+                    texto: "Você tem uma escolha?",
+                    afirmacao: [
+                        "Morreu de forma injusta, mas não sozinho. STATUS: Morto."
+                    ],
+                    
+                },
+                {
+                    texto: "Você tem uma escolha?",
+                    afirmacao: [
+                        "Morreu de forma injusta, mas não sozinho. STATUS: Morto."
+                    ],
+                    
+                },
+            ]
+        },
+        {
+            enunciado: "Sua sorte te salvou mais uma vez. Algo passou pelas árvores, correndo acima de tudo em seu caminho. Isso inclui o seu pequeno 'amigo' que te perseguiu, o levando também. Agora, da sua frente, há uma grande parede, alta o bastante para cobrir sua visão, mesmo depois de você olhar para cima. Tem uma porta a direita, alguns quilômetros dai.",
+            alternativas: [
+                {
+                    texto: "Andar a direita discretamente, sendo calmo.",
+                    afirmacao: [
+                        "Cuidadoso, até demais, mas salvou sua pele o bastante."
+                    ],
+                    proxima: 8,
+                },
+                {
+                    texto: "Ir a direita sem precaução, pois você não tem nada a temer.",
+                    afirmacao: [
+                        "Completo idiota."
+                    ],
+                    proxima: 7,
+                },
+            ]
+        },
+        {
+            enunciado: "As torretas ativaram. Seu corpo ficou cheio de balas em questões de segundos. Foi um erro ter saído de seu lar.",
+            alternativas: [
+                {
+                    texto: "Você tem uma escolha?",
+                    afirmacao: [
+                        "████ pensou que poderia ter um final feliz. STATUS: Morto."
+                    ],
+                },
+                {
+                    texto: "Você tem uma escolha?",
+                    afirmacao: [
+                        "████ pensou que poderia ter um final feliz. STATUS: Morto."
+                    ],
+                },
+            ]
+        },
+        {
+            enunciado: "A porta está ai, para ser aberta. Existe duas torretas acima, mas elas não podem te ver. Um paço afrente, pois sua curiosidade te chama. Você não pensa duas vezes, e abre a porta, a fechando atrás de você mesmo.",
+            alternativas: [
+                {
+                    texto: "Você tem uma outra escolha?",
+                    afirmacao: [
+                        "STATUS: Vivo"
+                    ]
+                },
+                {
+                    texto: "Você tem uma outra escolha?",
+                    afirmacao: [
+                        "STATUS: Vivo"
+                    ]
+                },
+            ]
+        },
+    ];
+    
+  
+
+
     {
         enunciado: "Você está na floresta. O que faz?",
         alternativas: [
     {
         texto: "Procura um abrigo.",
-        afirmacao: "Você encontra um abrigo seguro e continua sua jornada. No entanto, você sente que está sendo observado.",
+        afirmacao: [ "Você encontra um abrigo seguro e continua sua jornada. No entanto, você sente que está sendo observado.",
+    ]
         proxima: 1,
     },
     {
